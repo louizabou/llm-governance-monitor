e372d949aad89708f957e161e386a936fb592b18
Louiza <louizabou@gmail.com>
2025-12-22 20:00:35 -0500

    Remove test file

--- test_gemini.py
+++
@@ -1,7 +0,0 @@
-import vertexai
-from vertexai.generative_models import GenerativeModel
-
-vertexai.init(project='i-destiny-461017-g2', location='us-central1')
-model = GenerativeModel('gemini-2.0-flash-001')
-response = model.generate_content('Hello')
-print(response.text)
